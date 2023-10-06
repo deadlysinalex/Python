9ebfa3af8956750f411c5705bd9032dc6681ec23
deadlysinalex <114429974+deadlysinalex@users.noreply.github.com>
2023-10-06 17:44:48 -0400

    Pattern2.py

--- Pattern2.py
+++ Pattern2.py
@@ -1,5 +1,5 @@
 def loop():
-    print("What number do you want to use?")
+    print("What number do you want to use?")#asking user to give a number
     l = eval(input("your number: "))
     for i in range(0,l):
         print(i)
