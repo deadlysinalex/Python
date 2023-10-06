7f15472d22695000313d4c75a24b0ec2c62f130d
deadlysinalex <114429974+deadlysinalex@users.noreply.github.com>
2023-10-06 17:43:38 -0400

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,18 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> r=6#diameter
+>>> h=9#hight
+>>> a=2# length
+>>> circle=(math.pi)*r*r #Circle equation
+>>> cube=6*a*a # Cube equation
+>>> print(circle)#the output
+113.09733552923255#Circle area
+>>> print(cube)#prints the output of the cube
+24#cube area
+>>> cylinder=2*(math.pi)*r*h+2*(math.pi)*r*r # THIS IS THE EQUATION FOR A CYLINDER
+>>> print(cylinder)#This prints the output
+565.4866776461628#this is the output result
+>>> sphere=4*(math.pi)*r*r#Sphere equation
+>>> print(sphere)#prints the output
+452.3893421169302#result of output
---
+++ sum of series.py
@@ -0,0 +1,15 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=eval(input("enter number"))
+enter number 6
+>>> for i in [0,1,2,3,4,5,n]:
+...     print(i*i+i*i)
+... 
+...     
+0
+2
+8
+18
+32
+50
+72
